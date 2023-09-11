9d10ad5b2d62ffaea24249813f00e47f72d844fc
HicaroD <hdanrlley1@gmail.com>
2023-09-11 10:06:07 -0300

    feat: allocating the disk in memory

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include "stdio.h"
 #include "stdlib.h"
 
+#define DISK_SIZE 32
+
 typedef struct {
   char data;
   struct block *next;
@@ -8,13 +10,32 @@ typedef struct {
 
 Block *newBlock(char data) {
   Block *block = (Block *)malloc(sizeof(Block));
+  if (block == NULL) {
+    fprintf(stderr, "Unable to allocate a single block");
+    exit(1);
+  }
   block->data = data;
   block->next = NULL;
   return block;
 }
 
+Block *newDisk() {
+  Block *disk = (Block *)malloc(DISK_SIZE * sizeof(Block));
+  if (disk == NULL) {
+    fprintf(stderr, "Unable to allocate disk space");
+    exit(1);
+  }
+  for (int i = 0; i < DISK_SIZE; i++) {
+    disk[i].data = '0';
+  }
+  return disk;
+}
+
 int main() {
-  Block *firstBlock = newBlock('a');
-  printf("%c", firstBlock->data);
+  Block *disk = newDisk();
+  for (int i = 0; i < DISK_SIZE; i++)
+    printf("%c\n", disk[i].data);
+
+  free(disk); // NOTE: Na prática, você não iria desalocar o disco, obviamente
   return 0;
 }

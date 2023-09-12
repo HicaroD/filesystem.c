bad0a28e1594df2d71f748026418208c6916ea23
HicaroD <hdanrlley1@gmail.com>
2023-09-12 16:23:48 -0300

    feat: defining bitmap and checking if there is enough space

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include "stdio.h"
 #include "stdlib.h"
+#include <string.h>
 
 #define DISK_SIZE 32
 
-typedef struct {
+typedef struct block {
   char data;
   struct block *next;
 } block_t;
@@ -31,10 +32,74 @@ block_t *new_disk() {
   return disk;
 }
 
+int *get_bitmap() {
+  int *bitmap = (int *)malloc(DISK_SIZE * sizeof(int));
+  if (bitmap == NULL) {
+    fprintf(stderr, "Unable to allocate bitmap space");
+    exit(1);
+  }
+  for (int i = 0; i < DISK_SIZE; i++) {
+    bitmap[i] = 1;
+  }
+  return bitmap;
+}
+
+int has_enough_space_on_disk(int *bitmap, size_t file_length) {
+  size_t available_space_counter = 0;
+  for (size_t i = 0; i < DISK_SIZE; i++) {
+    if (bitmap[i])
+      available_space_counter++;
+
+    if (available_space_counter >= file_length)
+      return 1;
+  }
+  return 0;
+}
+
 int main() {
   block_t *disk = new_disk();
-  for (int i = 0; i < DISK_SIZE; i++)
-    printf("%c\n", disk[i].data);
+  int *bitmap = get_bitmap();
+
+  while (1) {
+    char input;
+    scanf("%s", &input);
+
+    if (input == 'E') {
+      printf("Exiting program...\n");
+      break;
+    }
+
+    switch (input) {
+    // READ
+    case 'R': {
+      printf("READ WORD\n");
+      break;
+    }
+    // WRITE
+    case 'W': {
+      char file[DISK_SIZE];
+      scanf("%s", file);
+      size_t file_length = strlen(file);
+
+      if (!has_enough_space_on_disk(bitmap, file_length)) {
+        printf("Insufficient memory.\n");
+        break;
+      }
+
+      // TODO: it has enough space, then store it
+      break;
+    }
+    // DELETE
+    case 'D': {
+      printf("DELETE WORD\n");
+      break;
+    }
+    default: {
+      printf("INVALID COMMAND");
+      break;
+    }
+    }
+  }
 
   free(disk); // NOTE: Na prática, você não iria desalocar o disco, obviamente
   return 0;
